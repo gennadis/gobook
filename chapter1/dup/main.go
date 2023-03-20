0c13b8317cd70e34f60d535c7c169ff22374014b
Gennady Badinov <gennady.badinov@gmail.com>
2023-03-20 20:04:04 +0300

    Update dup ex.

--- chapter1/dup/main.go
+++ chapter1/dup/main.go
@@ -1,5 +1,5 @@
-// Dup1 prints the text of each line that appears more than
-// once in the standard input, preceded by its count.
+// Dup2 prints the count and text of lines that appear more than once
+// in the input.  It reads from stdin or from a list of named files.
 package main
 
 import (
@@ -10,14 +10,31 @@ import (
 
 func main() {
 	counts := make(map[string]int)
-	input := bufio.NewScanner(os.Stdin)
-	for input.Scan() {
-		counts[input.Text()]++
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+		}
 	}
-	// NOTE: ignoring potential errors from input.Err()
 	for line, n := range counts {
 		if n > 1 {
 			fmt.Printf("%s: %d\n", line, n)
 		}
 	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	// NOTE: ignoring potential errors from input.Err()
 }
